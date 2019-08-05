31899f59210f9297e95c7179864e725cf21c4895
Xavier Fournet <461943+xfournet@users.noreply.github.com>
2019-08-06 01:16:47 +0200

    Fix sboot modules ordering

--- sboot/config/config.ps1
+++ sboot/config/config.ps1
@@ -1,5 +1,10 @@
 sboot_cfg -Name "Win10Settings"
 
+sboot_cfg -Name "ScoopConfig" -Default -Script {
+    . "$( sboot_mod "ScoopMod" )"
+    EnsureScoopConfig -ScoopConfig (Get-Content "$PSScriptRoot\ScoopConfig.json")
+}
+
 sboot_cfg -Name "ConCfg" -Script {
     $concfgFile = "$PSScriptRoot\concfg.json"
     if (Test-Path -LiteralPath $concfgFile) {
@@ -11,11 +16,6 @@ sboot_cfg -Name "ConCfg" -Script {
 }
 
 sboot_cfg -Name "EnvVars" -Default
-
-sboot_cfg -Name "ScoopConfig" -Default -Script {
-    . "$( sboot_mod "ScoopMod" )"
-    EnsureScoopConfig -ScoopConfig (Get-Content "$PSScriptRoot\ScoopConfig.json")
-}
 
 sboot_cfg -Name "SbootProfiles" -Default -Script {
     . "$( sboot_mod "SbootProfiles" )"
